ce387d6f027573148ad31ca85f663f90ab6b4d63
kulvirvirk <kvirkdev@gmail.com>
2020-02-24 05:38:23 +0000

    added the example of while loop

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-# 1. while loop example to print five numbers while condition is True
+# 1. give while loop example to print five numbers while condition is True
+# 2. give else statement example with while loop 
 
+# 1. give while loop example to print five numbers while condition is True
 counter = 1
 while counter < 6:
   print(counter)
   counter = counter + 1
+print('--------------******--------------\n')
+
