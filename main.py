eabd396a929d90940188cf2fa40004dca7679186
kulvirvirk <kvirkdev@gmail.com>
2020-02-25 01:50:16 +0000

    added an example of if statement combined with break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # 1. give while loop example to print five numbers while condition is True
 # 2. give else statement example with while loop
+# 3. give an example of using if and break with while loop
 
 # 1. give while loop example to print five numbers while condition is True
 counter = 1
@@ -16,3 +17,11 @@ while counter < 6:
 else:
     print('counter is no longer < 6')
 print('--------------******--------------\n')
+
+# 3. give an example of using if and break with while loop
+while True: 
+  response = input('Would you like to continue (y/n): ')
+  
+  if response == 'n' or response == 'N':
+    print('ok bye!')
+    break
